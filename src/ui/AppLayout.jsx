091bc0cc49b6ddf210950c87eaa9cab8a504890a
Tang Thien Dat <thiendattang@gmail.com>
2024-02-29 15:59:50 +0700

    Displaying a Loader Indicator

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,17 @@
+/* eslint-disable no-unused-vars */
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 
 function AppLayout() {
+    const navigation = useNavigation();
+    const isLoading = navigation.state === "loading";
+
+    console.log(navigation);
     return (
-        <div>
+        <div className="layout">
+            {isLoading && <Loader />}
             <Header />
             <main>
                 <h1>Content</h1>
