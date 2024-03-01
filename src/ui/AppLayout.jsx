4b3b1f33bfe52faf0ffdcb3eb7bb153ff9e6278a
Tang Thien Dat <thiendattang@gmail.com>
2024-03-01 20:20:12 +0700

    Styling Text

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -5,20 +5,19 @@ import { Outlet, useNavigation } from "react-router-dom";
 import Loader from "./Loader";
 
 function AppLayout() {
-    const navigation = useNavigation();
-    const isLoading = navigation.state === "loading";
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
 
-    return (
-        <div className="layout">
-            {isLoading && <Loader />}
-            <Header />
-            <main>
-                <h1>Content</h1>
-                <Outlet />
-            </main>
-            <CartOverview />
-        </div>
-    );
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <CartOverview />
+    </div>
+  );
 }
 
 export default AppLayout;
