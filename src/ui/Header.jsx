71417984499f13ef1b09bf4bf870a28202fe51f6
Tang Thien Dat <thiendattang@gmail.com>
2024-03-01 14:40:51 +0700

    Working With Color

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
-    return (
-        <header>
-            <Link to="/">Fast React Pizza Co.</Link>
-            <SearchOrder />
-            <p>Thien Dat</p>
-        </header>
-    );
+  return (
+    <header className="bg-yellow-500">
+      <Link to="/">Fast React Pizza Co.</Link>
+      <SearchOrder />
+      <p>Thien Dat</p>
+    </header>
+  );
 }
 
 export default Header;
