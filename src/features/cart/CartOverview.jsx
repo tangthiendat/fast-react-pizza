090a07aab2aad27e14a9c5a402c282ddae4271e3
Tang Thien Dat <thiendattang@gmail.com>
2024-03-01 21:23:44 +0700

    Responsive Design

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,7 +1,7 @@
 function CartOverview() {
   return (
-    <div className="bg-stone-800 p-4 text-stone-200">
-      <p className="space-x-4 font-semibold text-stone-300">
+    <div className="bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 md:text-base">
+      <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6 ">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
