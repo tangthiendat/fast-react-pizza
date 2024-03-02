46849d86b82be8ccb8818ba5bd5c5869734e7d86
Tang Thien Dat <thiendattang@gmail.com>
2024-03-02 09:52:58 +0700

    Styling Buttons Element State and Transition

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -5,105 +5,111 @@ import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
-    /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(str);
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+    str,
+  );
 
 const fakeCart = [
-    {
-        pizzaId: 12,
-        name: "Mediterranean",
-        quantity: 2,
-        unitPrice: 16,
-        totalPrice: 32,
-    },
-    {
-        pizzaId: 6,
-        name: "Vegetale",
-        quantity: 1,
-        unitPrice: 13,
-        totalPrice: 13,
-    },
-    {
-        pizzaId: 11,
-        name: "Spinach and Mushroom",
-        quantity: 1,
-        unitPrice: 15,
-        totalPrice: 15,
-    },
+  {
+    pizzaId: 12,
+    name: "Mediterranean",
+    quantity: 2,
+    unitPrice: 16,
+    totalPrice: 32,
+  },
+  {
+    pizzaId: 6,
+    name: "Vegetale",
+    quantity: 1,
+    unitPrice: 13,
+    totalPrice: 13,
+  },
+  {
+    pizzaId: 11,
+    name: "Spinach and Mushroom",
+    quantity: 1,
+    unitPrice: 15,
+    totalPrice: 15,
+  },
 ];
 
 function CreateOrder() {
-    const navigation = useNavigation();
-    // const [withPriority, setWithPriority] = useState(false);
-    const cart = fakeCart;
-    const isSubmitting = navigation.state === "submitting";
+  const navigation = useNavigation();
+  // const [withPriority, setWithPriority] = useState(false);
+  const cart = fakeCart;
+  const isSubmitting = navigation.state === "submitting";
 
-    const formErrors = useActionData();
-    return (
+  const formErrors = useActionData();
+  return (
+    <div>
+      <h2>Ready to order? Let&apos;s go!</h2>
+
+      {/* <Form method="POST" action="/order/new"> */}
+      {/* By default, React Router will match the closet route */}
+      <Form method="POST">
         <div>
-            <h2>Ready to order? Let&apos;s go!</h2>
-
-            {/* <Form method="POST" action="/order/new"> */}
-            {/* By default, React Router will match the closet route */}
-            <Form method="POST">
-                <div>
-                    <label>First Name</label>
-                    <input type="text" name="customer" required />
-                </div>
-
-                <div>
-                    <label>Phone number</label>
-                    <div>
-                        <input type="tel" name="phone" required />
-                    </div>
-                    {formErrors?.phone && <p>{formErrors.phone}</p>}
-                </div>
-
-                <div>
-                    <label>Address</label>
-                    <div>
-                        <input type="text" name="address" required />
-                    </div>
-                </div>
-
-                <div>
-                    <input
-                        type="checkbox"
-                        name="priority"
-                        id="priority"
-                        // value={withPriority}
-                        // onChange={(e) => setWithPriority(e.target.checked)}
-                    />
-                    <label htmlFor="priority">Want to yo give your order priority?</label>
-                </div>
-
-                <div>
-                    <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-                    <button disabled={isSubmitting}>
-                        {isSubmitting ? "Placing order..." : "Order now"}
-                    </button>
-                </div>
-            </Form>
+          <label>First Name</label>
+          <input type="text" name="customer" required />
         </div>
-    );
+
+        <div>
+          <label>Phone number</label>
+          <div>
+            <input type="tel" name="phone" required />
+          </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
+        </div>
+
+        <div>
+          <label>Address</label>
+          <div>
+            <input type="text" name="address" required />
+          </div>
+        </div>
+
+        <div>
+          <input
+            type="checkbox"
+            name="priority"
+            id="priority"
+            // value={withPriority}
+            // onChange={(e) => setWithPriority(e.target.checked)}
+          />
+          <label htmlFor="priority">Want to yo give your order priority?</label>
+        </div>
+
+        <div>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <button
+            disabled={isSubmitting}
+            className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+          >
+            {isSubmitting ? "Placing order..." : "Order now"}
+          </button>
+        </div>
+      </Form>
+    </div>
+  );
 }
 
 export async function action({ request }) {
-    const formData = await request.formData();
-    const data = Object.fromEntries(formData);
-    const order = {
-        ...data,
-        cart: JSON.parse(data.cart),
-        priority: data.priority === "on",
-    };
-    const errors = {};
-    if (!isValidPhone(order.phone)) {
-        errors.phone = "Please give us your correct phone number. We might need it to contact you.";
-    }
-    if (Object.keys(errors).length > 0) return errors;
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === "on",
+  };
+  const errors = {};
+  if (!isValidPhone(order.phone)) {
+    errors.phone =
+      "Please give us your correct phone number. We might need it to contact you.";
+  }
+  if (Object.keys(errors).length > 0) return errors;
 
-    const newOrder = await createOrder(order);
+  const newOrder = await createOrder(order);
 
-    return redirect(`/order/${newOrder.id}`);
+  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
