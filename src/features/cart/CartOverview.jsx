29207404ad88728480fdf21c1b81519fd1f1b984
Tang Thien Dat <thiendattang@gmail.com>
2024-03-05 07:57:29 +0700

    Reading and Updating the User State

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function CartOverview() {
   return (
     <div className="flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 md:text-base">
@@ -5,7 +7,7 @@ function CartOverview() {
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
-      <a href="#">Open cart &rarr;</a>
+      <Link to="/cart">Open cart &rarr;</Link>
     </div>
   );
 }
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -25,7 +25,7 @@ async function fetchAddress() {
 }
 */
 const initialState = {
-  username: "Dat",
+  username: "",
 };
 const userSlice = createSlice({
   name: "user",
