f2e3f0bd416503f95ee4ed013787f0c33963a9fa
Tang Thien Dat <thiendattang@gmail.com>
2024-03-05 14:31:42 +0700

    Updating Cart Quantities

---
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+import { useDispatch } from "react-redux";
+import Button from "../../ui/Button";
+import { decreaseItemQuantity, increaseItemQuantity } from "./cartSlice";
+
+/* eslint-disable react/prop-types */
+function UpdateItemQuantity({ pizzaId, currentQuantity }) {
+  const dispatch = useDispatch();
+  return (
+    <div className="flex items-center gap-2 md:gap-3">
+      <Button
+        type="round"
+        onClick={() => dispatch(increaseItemQuantity(pizzaId))}
+      >
+        +
+      </Button>
+      <span className="text-sm">{currentQuantity}</span>
+      <Button
+        type="round"
+        onClick={() => dispatch(decreaseItemQuantity(pizzaId))}
+      >
+        -
+      </Button>
+    </div>
+  );
+}
+
+export default UpdateItemQuantity;
